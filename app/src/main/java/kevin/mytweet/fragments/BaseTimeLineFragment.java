98b329d5c91ac7851e2fd4fb04538b948afb8fd7
KevFan <kevin_fan@hotmail.co.uk>
2017-12-28 00:55:20 +0000

    seperate out timelime adapter + correct update adapter data on fragement change

--- app/src/main/java/kevin/mytweet/fragments/BaseTimeLineFragment.java
+++ app/src/main/java/kevin/mytweet/fragments/BaseTimeLineFragment.java
@@ -22,6 +22,7 @@ import java.util.List;
 import kevin.mytweet.R;
 import kevin.mytweet.activities.AddTweetActivity;
 import kevin.mytweet.activities.DetailTweetPagerActivity;
+import kevin.mytweet.adapters.TimeLineAdapter;
 import kevin.mytweet.app.MyTweetApp;
 import kevin.mytweet.helpers.IntentHelper;
 import kevin.mytweet.models.Tweet;
@@ -91,73 +92,6 @@ public abstract class BaseTimeLineFragment extends Fragment implements AdapterVi
     return view;
   }
 
-  /**
-   * Custom adaptor for the timeline fragment to list tweets
-   */
-  class TimeLineAdapter extends ArrayAdapter<Tweet> {
-    private Context context;
-    protected List<Tweet> timeLine;
-
-    /**
-     * TimeLineAdapter constructor
-     *
-     * @param context Context of where the adapter is constructed
-     * @param tweets  ArrayList of tweets
-     */
-    private TimeLineAdapter(Context context, List<Tweet> tweets) {
-      super(context, 0, tweets);
-      this.context = context;
-      this.timeLine = tweets;
-    }
-
-    /**
-     * Call list_item_tweet for each tweet in ArrayList to display tweet data at specific position
-     *
-     * @param position    position of tweet item
-     * @param convertView View to reuse
-     * @param parent      View parent of where convert view will be attached
-     * @return View with tweet data at specific position
-     */
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-      LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-      if (convertView == null) {
-        convertView = inflater.inflate(R.layout.list_item_tweet, null);
-      }
-
-      Tweet tweet = timeLine.get(position);
-
-      TextView tweetText = (TextView) convertView.findViewById(R.id.list_item_tweetText);
-      tweetText.setText(tweet.tweetText);
-      tweetText.setMaxLines(1);
-
-      TextView tweetDate = (TextView) convertView.findViewById(R.id.list_item_tweetDate);
-      tweetDate.setText(tweet.tweetDate.toString());
-
-      TextView tweetUserName = (TextView) convertView.findViewById(R.id.list_item_tweetUserName);
-      tweetUserName.setText(tweet.tweetUser.firstName + " " + tweet.tweetUser.lastName);
-
-      ImageView tweetUserImage = (ImageView) convertView.findViewById(R.id.list_item_tweetUser_image);
-      if (!tweet.tweetUser.image.equals("")) {
-        Picasso.with(getActivity()).load(tweet.tweetUser.image).into(tweetUserImage);
-      } else {
-        tweetUserImage.setImageResource(R.mipmap.ic_launcher_round);
-      }
-
-      ImageView tweetImage = (ImageView) convertView.findViewById(R.id.list_item_tweetImage);
-      if (!tweet.tweetImage.equals("")) {
-        Picasso.with(getActivity()).load(tweet.tweetImage).into(tweetImage);
-      }
-
-      return convertView;
-    }
-
-    @Override
-    public int getCount() {
-      return timeLine.size();
-    }
-  }
-
   public void setNoTweetMessage() {
     if (!adapter.timeLine.isEmpty()) {
       noTweetMessage.setVisibility(View.INVISIBLE);
@@ -167,8 +101,9 @@ public abstract class BaseTimeLineFragment extends Fragment implements AdapterVi
   }
 
   public void updateTimeLineData(List<Tweet> updatedTimeline) {
-//    app.timeLine = updatedTimeline;
-    adapter.timeLine = updatedTimeline;
+    app.timeLine = updatedTimeline;
+    adapter = new TimeLineAdapter(getActivity(), updatedTimeline);
+    listView.setAdapter(adapter);
     setNoTweetMessage();
     adapter.notifyDataSetChanged();
   }
