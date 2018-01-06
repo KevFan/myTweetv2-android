f3c48a7c1adf07b228211c1706586c0f05b4a86a
KevFan <kevin_fan@hotmail.co.uk>
2018-01-06 14:18:45 +0000

    comments for models

--- app/src/main/java/kevin/mytweet/models/Coords.java
+++ app/src/main/java/kevin/mytweet/models/Coords.java
@@ -1,6 +1,9 @@
 package kevin.mytweet.models;
 
+/**
+ * Co-ordinates model to hold latitude and longitude
+ */
 public class Coords {
-    public double latitude;
-    public double longitude;
+  public double latitude;
+  public double longitude;
 }
